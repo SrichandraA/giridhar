8c55cdf101b50512e3f91e6ea0fae22275785cec
SrichandraA <chandra.urfrnd@gmail.com>
2018-07-19 00:33:21 +0530

    navbar

--- scripts.js
+++ scripts.js
@@ -1,27 +1,40 @@
 
 $(document).ready(function(){
-  if($(window).scrollTop() === 0){
-    $("nav").css("opacity","0.3");
-  // $("nav").css("background","linear-gradient(to right,  rgba(108, 72, 169, 0.3) 0%,rgba(255, 40, 122, 0.3) 100%)");
-  }
-  else{
-    $("nav").css("opacity","1");
-    $(".nav-name").css("margin-left","0");
+    if (screen && screen.width > 1024) {
+        if ($(window).scrollTop() === 0) {
+            $("nav").css("opacity", "0.3");
+            // $("nav").css("background","linear-gradient(to right,  rgba(108, 72, 169, 0.3) 0%,rgba(255, 40, 122, 0.3) 100%)");
+        }
+        else {
+            $("nav").css("opacity", "1");
+            $(".nav-name").css("margin-left", "0");
 
-  }
-
-  $(window).scroll(function(){
-  	var scroll = $(window).scrollTop();
-    if(scroll<560){
-      $(".nav-name").css("margin-left",560-scroll);
-      $("nav").css("opacity",0.3+0.001*scroll);
-
+        }
+    }
+    else {
+        $("nav").css("opacity", "1");
+        $(".nav-name").css("margin-left", "0");
+        $(".nav-name").css("margin-right", "0");
 
     }
-	  else if (scroll > 560) {
-      $("nav").css("opacity",1);
 
-	  }
+  $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+
+      if (screen && screen.width > 1024) {
+
+          if (scroll < 560) {
+              $(".nav-name").css("margin-left", 560 - scroll);
+              $("nav").css("opacity", 0.3 + 0.001 * scroll);
+
+
+          }
+          else if (scroll > 560) {
+              $("nav").css("opacity", 1);
+
+          }
+      }
+
     $('#nav-resume').toggleClass('active-keep',
      //add 'ok' class when div position match or exceeds else remove the 'ok' class.
       scroll >= $('#resume').offset().top-200,
