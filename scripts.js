a649e1135dbcc1bc3c26432dd37052709e6b8909
SrichandraA <chandra.urfrnd@gmail.com>
2018-07-18 20:42:05 +0530

    navbar

--- scripts.js
+++ scripts.js
@@ -12,16 +12,16 @@ $(document).ready(function(){
 
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
-    // if(scroll<560){
-    //   $(".nav-name").css("margin-left",560-scroll);
-    //   $("nav").css("opacity",0.3+0.001*scroll);
-    //
-    //
-    // }
-	 //  else if (scroll > 560) {
-    //   $("nav").css("opacity",1);
-    //
-	 //  }
+    if(scroll<560){
+      $(".nav-name").css("margin-left",560-scroll);
+      $("nav").css("opacity",0.3+0.001*scroll);
+
+
+    }
+	  else if (scroll > 560) {
+      $("nav").css("opacity",1);
+
+	  }
     $('#nav-resume').toggleClass('active-keep',
      //add 'ok' class when div position match or exceeds else remove the 'ok' class.
       scroll >= $('#resume').offset().top-200,
